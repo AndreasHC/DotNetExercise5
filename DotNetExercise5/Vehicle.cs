72f70aef81ba0af3dcb24b906fe096b592f2c4b7
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-03-21 09:22:55 +0100

    Describing vehicles. Untested for now.

--- DotNetExercise5/Vehicle.cs
+++ DotNetExercise5/Vehicle.cs
@@ -2,5 +2,25 @@
 {
     internal class Vehicle : IVehicle
     {
+        private string RegistrationNumber { get; init; }
+        private VehicleColor Color { get; init; }
+        private uint NumberOfWheels { get; init; }
+
+        public Vehicle(string registrationNumber, VehicleColor color, uint numberOfWheels)
+        {
+            RegistrationNumber = registrationNumber;
+            Color = color;
+            NumberOfWheels = numberOfWheels;
+        }
+
+        protected virtual string TypeDescription()
+        {
+            return "Obeskrivligt fordon";
+        }
+
+        public override string ToString()
+        {
+            return $"{TypeDescription()}{Environment.NewLine}Registreringsnummer: {RegistrationNumber}{Environment.NewLine}Färg: {Color.Swedish()}{Environment.NewLine}Antal hjul: {NumberOfWheels}";
+        }
     }
 }
---
+++ DotNetExercise5/VehicleColor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Immutable;
+
+namespace DotNetExercise5
+{
+    internal enum VehicleColor
+    {
+        // Copied from console color, because it should be possible to change independently.
+        Black,
+        DarkBlue,
+        DarkGreen,
+        DarkCyan,
+        DarkRed,
+        DarkMagenta,
+        DarkYellow,
+        Gray,
+        DarkGray,
+        Blue,
+        Green,
+        Cyan,
+        Red,
+        Magenta,
+        Yellow,
+        White
+    }
+    internal static class VehicleColorMethods
+    {
+        private static ImmutableDictionary<VehicleColor, string> SwedishDictionary { get; } =
+            new Dictionary<VehicleColor, string>()
+            { {VehicleColor.Black, "Svart"},
+              {VehicleColor.DarkBlue, "Mörkblå"},
+              {VehicleColor.DarkGreen, "Mörkgrön"},
+              {VehicleColor.DarkCyan, "Mörk cyan"},
+              {VehicleColor.DarkRed, "Mörkröd"},
+              {VehicleColor.DarkMagenta, "Mörk magenta"},
+              {VehicleColor.DarkYellow, "Mörkgul"},
+              {VehicleColor.Gray, "Grå"},
+              {VehicleColor.DarkGray, "Mörkgrå"},
+              {VehicleColor.Blue, "Blå"},
+              {VehicleColor.Green, "Grön"},
+              {VehicleColor.Cyan, "Cyan"},
+              {VehicleColor.Red, "Röd"},
+              {VehicleColor.Magenta, "Magenta"},
+              {VehicleColor.Yellow, "Gul"},
+              {VehicleColor.White, "Vit"},
+            }.ToImmutableDictionary();
+        internal static string Swedish(this VehicleColor color)
+        {
+            return SwedishDictionary[color];
+        }
+    }
+}
