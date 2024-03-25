8fd50cfeaf42a88d11757a08836f2085b38d4f68
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-03-25 16:46:40 +0100

    Spelling

--- DotNetExercise5/VehicleSearch.cs
+++ DotNetExercise5/VehicleSearch.cs
@@ -5,7 +5,7 @@ namespace DotNetExercise5
     internal class VehicleSearch
     {
         private IEnumerable<IVehicle> DataSet { get; init; }
-        private List<VehicleCriterion> Critera { get; init; } = new List<VehicleCriterion>();
+        private List<VehicleCriterion> Criteria { get; init; } = new List<VehicleCriterion>();
         internal VehicleSearch(IEnumerable<IVehicle> dataSet)
         {
             DataSet = dataSet;
@@ -13,7 +13,7 @@ namespace DotNetExercise5
         internal IEnumerable<IVehicle> Run()
         {
             IEnumerable<IVehicle> data = DataSet;
-            foreach (VehicleCriterion criterion in Critera)
+            foreach (VehicleCriterion criterion in Criteria)
             {
                 data = data.Where(criterion.FilterFunction);
             }
@@ -21,13 +21,13 @@ namespace DotNetExercise5
         }
         internal void AddCriterion(VehicleCriterion criterion)
         {
-            Critera.Add(criterion);
+            Criteria.Add(criterion);
         }
 
         public override string ToString()
         {
             StringBuilder buffer = new StringBuilder();
-            foreach (VehicleCriterion criterion in Critera)
+            foreach (VehicleCriterion criterion in Criteria)
             {
                 buffer.Append(criterion.ToString());
                 buffer.Append(Environment.NewLine);
